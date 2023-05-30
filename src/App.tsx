c63340c5a5203e7e6ef349702f742a1edb609432
minbeom <alsqja17@naver.com>
2023-05-30 23:59:48 +0900

    feat: #5 웹 템플릿 추가
    웹 템플릿 추가
    
    close #5

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useSelector } from 'react-redux';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { ThemeProvider, Theme } from '@mui/material/styles';
+import { CssBaseline, StyledEngineProvider } from '@mui/material';
+
+// routing
+import Routes from 'routes';
+
+// store
+import { DefaultRootStateProps } from 'types';
+
+// defaultTheme
+import themes from 'themes';
+
+// project imports
+import Locales from 'ui-component/Locales';
+import NavigationScroll from 'layout/NavigationScroll';
+// import RTLLayout from 'ui-component/RTLLayout';
+import Snackbar from 'ui-component/extended/Snackbar';
+
+// auth provider
+import { FirebaseProvider } from 'contexts/FirebaseContext';
+// import { JWTProvider } from 'contexts/JWTContext';
+// import { Auth0Provider } from 'contexts/Auth0Context';
+
+declare module '@mui/styles/defaultTheme' {
+    // eslint-disable-next-line @typescript-eslint/no-empty-interface
+    interface DefaultTheme extends Theme {}
 }
+
+// ==============================|| APP ||============================== //
+
+const App = () => {
+    const customization = useSelector((state: DefaultRootStateProps) => state.customization);
+
+    return (
+        <StyledEngineProvider injectFirst>
+            <ThemeProvider theme={themes(customization)}>
+                <CssBaseline />
+                {/* RTL layout */}
+                {/* <RTLLayout> */}
+                <Locales>
+                    <NavigationScroll>
+                        <FirebaseProvider>
+                            <>
+                                <Routes />
+                                <Snackbar />
+                            </>
+                        </FirebaseProvider>
+                    </NavigationScroll>
+                </Locales>
+                {/* </RTLLayout> */}
+            </ThemeProvider>
+        </StyledEngineProvider>
+    );
+};
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+// third party
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+
+// load mock apis
+import '_mockApis';
+
+// project imports
+import * as serviceWorker from 'serviceWorker';
+import App from 'App';
+import { store, persister } from 'store';
+
+// style + assets
+import 'assets/scss/style.scss';
+import config from 'config';
+
+// ==============================|| REACT DOM RENDER  ||============================== //
+
+ReactDOM.render(
+    <Provider store={store}>
+        <PersistGate loading={null} persistor={persister}>
+            <BrowserRouter basename={config.basename}>
+                <App />
+            </BrowserRouter>
+        </PersistGate>
+    </Provider>,
+    document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
